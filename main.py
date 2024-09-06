6878c25f27a928086457e6635a087c027776b7eb
Dibas02 <debal1952@gmail.com>
2024-09-06 20:14:09 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+msg = "Welcome to Codingal"
+print("The fourth letter is", msg[3])
+print("The length of the message is", len(msg))
+print("substring", msg[0:7])
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+print(msg.count("e"))
+print(msg.find("C"))
